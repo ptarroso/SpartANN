3dcc4dc9adcd805d01f1db37914d02fe2231db50
Pedro Tarroso <ptarroso@cibio.up.pt>
2025-01-24 10:01:33 +0000

    added docstrng

--- spartann/version.py
+++ spartann/version.py
@@ -1,2 +1,7 @@
+"""
+SpartANN - Spectral Pattern Analysis and Remote-sensing Tool with Artificial Neural Networks
+Copyright (C) 2025 Pedro Tarroso
+
+"""
 __all__ = ["__version__"]
 __version__ = "1.0"
