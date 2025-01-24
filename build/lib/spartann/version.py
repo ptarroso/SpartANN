70a6ad2d80f684de5fa99f2f4e7ea1ac2f4d021d
Pedro Tarroso <ptarroso@cibio.up.pt>
2025-01-24 10:42:01 +0000

    latest build

--- build/lib/spartann/version.py
+++ build/lib/spartann/version.py
@@ -2,18 +2,6 @@
 SpartANN - Spectral Pattern Analysis and Remote-sensing Tool with Artificial Neural Networks
 Copyright (C) 2025 Pedro Tarroso
 
-This program is free software: you can redistribute it and/or modify it under the terms
-of the GNU General Public License as published by the Free Software Foundation, either
-version 3 of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <https://www.gnu.org/licenses/>.
-
 """
 __all__ = ["__version__"]
 __version__ = "1.0"
